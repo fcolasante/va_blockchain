7e3d056b76ed7e49489dee9944b11a58fbceeb23
zascerta <giammarco.daless@gmail.com>
2021-04-11 10:23:51 +0200

    robe vecchie

--- viz-tool/stackedBar/stacked_v5_test.js
+++ viz-tool/stackedBar/stacked_v5_test.js
@@ -12,7 +12,7 @@ var svg = d3.select("#chart2")
 
 // to read dates properly
 var parseDate = d3.timeParse("%Y/%m"),    // "%d/%m/%Y" => DD/MM/YY
-    formatDate = d3.timeFormat("%m-%y");
+    formatDate = d3.timeFormat("%y");
 
 // Parse the Data
 d3.csv("stacked_bar_months.csv").then(function(data) {
@@ -27,25 +27,16 @@ d3.csv("stacked_bar_months.csv").then(function(data) {
     var groups = d3.map(data, function(d){ return d.date; }).keys()
     console.log(groups)
 
-    // Add X axis
+    // Create X axis
     var x = d3.scaleBand()
         .domain(groups)
         .range([0, width])
         .padding([0.1]);   
 
-    // Add Y axis
+    // Create Y axis
     var y = d3.scaleLinear()
         .domain([0, 150000])
         .range([height, 0]);
-
-    svg.append("g")
-        .attr("class", "x-axis") 
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x).tickSizeOuter(0).tickFormat(d3.timeFormat("%y")));
-        
-    svg.append("g")
-        .attr("class", "y-axis")
-        .call(d3.axisLeft(y));
 
     // color palette = one color per subgroup
     var color = d3.scaleOrdinal()
@@ -55,20 +46,70 @@ d3.csv("stacked_bar_months.csv").then(function(data) {
     //stack the data? --> stack per subgroup
     var stackedData = d3.stack()
         .keys(subgroups)(data);
+    //console.log(stackedData)
 
+
+    // -----------------------------------
+    // Create a tooltip
+    // -----------------------------------
+    var tooltip = d3.select("#chart2")
+        .append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 1)
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "1px")
+        .style("border-radius", "5px")
+        .style("padding", "10px");
+
+    // Three function that change the tooltip when user hover / move / leave a cell
+    var mouseover = function(d) {
+        var subgroupName = d3.select(this.parentNode).datum().key;
+        var subgroupValue = d.data[subgroupName];
+        tooltip.html("subgroup: " + subgroupName + "<br>" + "Value: " + subgroupValue)
+            .style("opacity", 1);
+    };
+    // It is important to put the +90: other wise the tooltip is exactly 
+    // where the point is an it creates a weird effect
+    var mousemove = function(d) {
+        tooltip.style("left", (d3.mouse(this)[0]+90) + "px") 
+            .style("top", (d3.mouse(this)[1]) + "px");
+    };
+    var mouseleave = function(d) { tooltip.style("opacity", 0); };
+
+    // ----------------------------------------------
     // Show the bars
+    // ----------------------------------------------
     svg.append("g")
-        .selectAll("g")
         // Enter in the stack data = loop key per key = group per group
+        .selectAll("g")
         .data(stackedData)
         .enter().append("g")
+            // enter a second time = loop subgroup per subgroup to add all rectangles
+            .attr("class", "crypto")
             .attr("fill", function(d) { return color(d.key); })
             .selectAll("rect")
-            // enter a second time = loop subgroup per subgroup to add all rectangles
             .data(function(d) { return d; })
             .enter().append("rect")
                 .attr("x", function(d) { return x(d.data.date); })
                 .attr("y", function(d) { return y(d[1]); })
                 .attr("height", function(d) { return y(d[0]) - y(d[1]); })
-                .attr("width", x.bandwidth());
+                .attr("width", x.bandwidth())
+                .on("mouseover", mouseover)
+                .on("mousemove", mousemove)
+                .on("mouseleave", mouseleave);
+
+    // Add x-axis
+    svg.append("g")
+        .attr("class", "x-axis") 
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x).tickSizeOuter(0).tickFormat(formatDate));
+        //.selectAll("text")
+        //    .attr("transform", "translate(-10,0)rotate(-45)")
+        //    .style("text-anchor", "end");
+
+    // Add y-axis
+    svg.append("g")
+        .attr("class", "y-axis")
+        .call(d3.axisLeft(y));
 });
